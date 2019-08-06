032a9770867fe8a18c341a417be6a536ffb01801
LucaNoto <lucanoto@MacBook-Pro-de-Luca-Noto.local>
2019-08-06 08:44:26 -0300

    jhjh

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // Example programs
 #include "programs/examples/ex01_leds.h"
 //#include "programs/examples/ex02_motors.h"
-//#include "programs/examples/ex03_serial.h"
+//#include "programs/examples/ex03_serial.h" hjjh
 //#include "programs/examples/ex04_imu.h"
 //#include "programs/examples/ex05_range.h"
 //#include "programs/examples/ex06_flow.h"
